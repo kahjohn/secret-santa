8f53104d5b575a5549463bd55bd598b608f1c076
kahjohn <93301900+kahjohn@users.noreply.github.com>
2024-12-08 15:14:16 +0000

    DEV: push first version code to github

---
+++ santa.py
@@ -0,0 +1,85 @@
+import numpy as np
+import pandas as pd
+import random
+
+#If the list is taken from a Google spreadsheet
+def get_list(file):
+    """Get the list of participants from a single column csv file"""
+    # participants = pandas.read_csv(file, header=None, 
+    #                                names=np.array(['Names']), encoding='utf-8')
+    # participants = list(participants['Names'])
+    participants = file
+    return (participants)
+
+def get_special_code(final_list):
+
+    random.seed(42)
+    special_code = []
+
+    # Generate a random 5-digit number
+
+    for i in range(0,len(final_list)):
+        random_number = random.randint(10000, 99999)
+        special_code.append(random_number)
+
+    return special_code
+
+def get_secret_santa(participants):
+    """Generate a draw from a list of participants using numpy arrays and random methods
+    to define who offers/receives a gift to/from whom """
+    to_pickup = np.array([participants.index(name) for name in participants])  #Int paired with each participant
+    print(str(len(to_pickup))+' participants')
+
+    #Initialisation tirage au sort
+    pickedup = np.array([])
+    results = ""
+    results_raw = []
+
+    np.random.seed(3690)
+    r = np.random.choice(to_pickup)
+    print("picked-up first: " + participants[r] + '(' + str(r) + ')' + '\n')
+    to_pickup = np.delete(to_pickup, np.where(to_pickup == r))
+    pickedup = np.append(pickedup, r)
+
+    #Who offers to who
+    for person in to_pickup:  #N-1 tirages au sort pour N participants
+        r = np.random.choice(to_pickup)
+        pickedup = np.append(pickedup, r)
+        to_pickup = np.delete(to_pickup, np.where(to_pickup == r))
+        results = results + participants[int(pickedup.item(-2))] + ' offers to ' + participants[r] + '\n'
+        # print([participants[int(pickedup.item(-2))], participants[r]])
+        results_raw.append(list([participants[int(pickedup.item(-2))], participants[r]]))
+
+    #Final - results + last picked up offers to first picked-up
+    results = results + 'And ' + participants[int(pickedup.item(-1))] + ' offers to ' + participants[int(pickedup.item(0))] + '\n'
+    results_raw.append(list([participants[int(pickedup.item(-1))], participants[int(pickedup.item(0))]]))
+    # print([participants[int(pickedup.item(-2))], participants[r]])
+
+    # print(results)
+    # print(results_raw)
+    
+    # #Copy in the clipboard
+    # pyperclip.copy(results)
+    # clipboard = pyperclip.paste()
+    # print('Results of the draw copied in the clipboard')
+
+    return results_raw
+
+participant_list = ['KJ','KW','JN','JL']
+
+lst = get_list(participant_list)
+# print(lst)
+
+final = get_secret_santa(lst)
+# print(final)
+
+code = get_special_code(lst)
+# print(code)
+
+tbl1 = pd.DataFrame(final, columns=['Santa','Recipient'])
+tbl2 = pd.DataFrame(code, columns=['Code'])
+
+tbl = pd.merge(tbl1, tbl2, how='left', left_index=True, right_index=True)
+print(tbl[['Santa','Code']])
+
+tbl.to_csv('/workspaces/secret-santa/santa.csv', index=False)
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,34 @@
 import streamlit as st
+import pandas as pd
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+def get_name(code):
+    output = pd.read_csv('/workspaces/secret-santa/santa.csv')
+    # print(output)
+    recepient = output[ output['Code'] == code ]['Recipient'].to_numpy()[0]
+    # print(recepient)
+    return 'Congratz, you are going to buy a Christmas gift for ' + recepient
+
+# st.title("My new app")
+# st.write(
+#     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+# )
+
+st.image('/workspaces/secret-santa/santa.png')
+st.title("Secret Santa")
+st.subheader("Find out who you need to buy the gift for.")
+
+# Get the user's input
+user_input = st.text_input("Key in your special code: ")
+submit_button = st.button("Submit")
+
+# Convert the input to a float (if valid)
+try:
+    user_input = float(user_input)
+
+    # Generate a response
+    if user_input or submit_button:
+        response = get_name(user_input)
+        st.write(response)
+
+except ValueError:
+    st.write("Please enter a valid number.")
